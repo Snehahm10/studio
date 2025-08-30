2734fbb3df13e03637a5fb0464d862eeca923b96
Sneha Hm <snehahanjgimath@gmail.com>
2025-08-30 20:20:51 +0000

    see

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,14 @@ import { initializeApp, getApp, getApps, FirebaseApp } from "firebase/app";
 import { getStorage, ref, listAll, getDownloadURL, getMetadata, updateMetadata, ListResult, StorageReference, getBytes, deleteObject } from "firebase/storage";
 import { Subject, ResourceFile } from "./data";
 
+// Correct and verified Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyAWfHPsEOU50o5oLLNKF0YycHlyNF6gjKA",
+  authDomain: "vtu-assistant-rkmf1.firebaseapp.com",
+  projectId: "vtu-assistant-rkmf1",
+  storageBucket: "vtu-assistant-rkmf1.appspot.com",
+  messagingSenderId: "998939257663",
+  appId: "1:998939257663:web:1b33f669b722eaa991c18a",
 };
 
 
