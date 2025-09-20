b9c87c3b3453fd3f384f1e0f1aeee5492f6b29a8
Sneha Hm <snehahanjgimath@gmail.com>
2025-09-20 08:56:22 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps, FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAWfHPsEOU50o5oLLNKF0YycHlyNF6gjKA",
-  authDomain: "vtu-assistant-rkmf1.firebaseapp.com",
-  projectId: "vtu-assistant-rkmf1",
-  storageBucket: "vtu-assistant-rkmf1.appspot.com",
-  messagingSenderId: "998939257663",
-  appId: "1:998939257663:web:1b33f669b722eaa991c18a"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 
@@ -24,3 +25,4 @@ if (getApps().length === 0) {
 const auth = getAuth(app);
 
 export { app, auth };
+
