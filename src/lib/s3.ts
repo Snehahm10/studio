fd1cbc73bd9a5f3b9fbd1a335c2d31cdc46e0bd4
Sneha Hm <snehahanjgimath@gmail.com>
2025-11-18 14:48:56 +0000

    see

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -5,7 +5,7 @@ import { S3Client, PutObjectCommand, ListObjectsV2Command, GetObjectCommand, Del
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 const s3Client = new S3Client({
-  region: 'eu-north-1',
+  region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
